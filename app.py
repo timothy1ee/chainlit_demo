90b0c23d047dae32c1897047e52cb6bc02cc8293
Tim Lee <tim@codepath.org>
2024-08-17 10:01:55 -0700

    Support attaching images to a chat message

--- app.py
+++ app.py
@@ -1,28 +1,55 @@
 import chainlit as cl
 import openai
 import os
+import base64
 
 api_key = os.getenv("OPENAI_API_KEY")
-
+# api_key = "R4019I02UE1CXF68L6UDO1H9O2IQAJOOW9ELO9SW"
 endpoint_url = "https://api.openai.com/v1"
+# endpoint_url = "https://api.runpod.ai/v2/5xaa0o8c13a55l/openai/v1"
 client = openai.AsyncClient(api_key=api_key, base_url=endpoint_url)
 
+# https://platform.openai.com/docs/models/gpt-4o
 model_kwargs = {
     "model": "chatgpt-4o-latest",
     "temperature": 0.3,
     "max_tokens": 500
 }
+# model_kwargs = {
+#     "model": "mistralai/Mistral-7B-Instruct-v0.3",
+#     "temperature": 0.3,
+#     "max_tokens": 500
+# }
 
 @cl.on_message
 async def on_message(message: cl.Message):
     # Maintain an array of messages in the user session
     message_history = cl.user_session.get("message_history", [])
-    message_history.append({"role": "user", "content": message.content})
 
-    response = await client.chat.completions.create(
-        messages=[{"role": "user", "content": message.content}],
-        **model_kwargs
-    )
+    # Processing images exclusively
+    images = [file for file in message.elements if "image" in file.mime] if message.elements else []
+
+    if images:
+        # Read the first image and encode it to base64
+        with open(images[0].path, "rb") as f:
+            base64_image = base64.b64encode(f.read()).decode('utf-8')
+        message_history.append({
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": message.content if message.content else "What’s in this image?"
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/jpeg;base64,{base64_image}"
+                    }
+                }
+            ]
+        })
+    else:
+        message_history.append({"role": "user", "content": message.content})
 
     response_message = cl.Message(content="")
     await response_message.send()
